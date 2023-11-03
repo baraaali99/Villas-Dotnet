fe7dc4c534deb22472aaa81cd0233be812ce8fbc
baraaali99 <baraa.ali1912@gmail.com>
2023-11-04 00:21:56 +0400

    Added new Migrations

---
+++ Migrations/20231103184801_AddVillaNumbersTable.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace firstDotnetProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVillaNumbersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Amenity",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateTable(
+                name: "VillaNumbers",
+                columns: table => new
+                {
+                    VillaNo = table.Column<int>(type: "int", nullable: false),
+                    SpecialDetails = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreateDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VillaNumbers", x => x.VillaNo);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VillaNumbers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Amenity",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
